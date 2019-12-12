806b645f2aea4e627156cbc982741fe9196d508c
Sangbum Woo <sbwoo87@gmail.com>
2019-12-13 06:20:48 +0900

    Organization Module Load

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { OrganizationsModule } from './organizations/organizations.module';
 import config from './config/keys';
 
 @Module({
-  imports: [MongooseModule.forRoot(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }), UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [MongooseModule.forRoot(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }), UsersModule, OrganizationsModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule { }
--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,5 +1,5 @@
 export default {
     // mongoURI: 'mongodb+srv://foxpro87:cluster0-dsoxx.mongodb.net/test?retryWrites=true&w=majority'
-    mongoURI: 'mongodb+srv://foxpro87:foxpro87@cluster0-dsoxx.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://foxpro87:foxpro87@cluster0-dsoxx.mongodb.net/test?retryWrites=true&w=majority',
     //         mongodb+srv://foxpro87:<password>@cluster0-dsoxx.mongodb.net/test?retryWrites=true&w=majority
-}
+};
